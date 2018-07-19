206bfa018a0f85066ed318d14f30257c2f8727e9
Heyder Dias <heyderpd@gmail.com>
2018-07-18 22:30:35 -0300

    add clear

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import { Howl } from 'howler'
 
 let timeoutId
 let howl
+let lastEvents
 
 const start = (Sound, Events) => {
   /* https://www.npmjs.com/package/howler */
   howl = new Howl({ src: [Sound], html5: true })
-  Events.map(
-    event => document.addEventListener(event, keyDown, false))
+  Events.map(event => document.addEventListener(event, keyDown, false))
+  lastEvents = Events
 }
 
-const timeoutToPause = () => {
+const timeoutToPause = _ => {
   clearTimeout(timeoutId)
   timeoutId = setTimeout(() => {
     howl.pause()
@@ -24,11 +25,20 @@ const keyDown = e => {
   }
 }
 
+const clear = _ => {
+  if (Array.isArray(lastEvents))
+    lastEvents.map(event => document.removeEventListener(event, keyDown))
+  lastEvents = null
+}
+
 const aquidauana = (Sound = './guitarra.mp3', Events = ['keydown', 'mousemove', 'scroll', 'click']) => {
   if (!document) {
     throw 'add this in document html'
   }
+  clear()
   start(Sound, Events)
 }
+
+aquidauana.clear = clear
 
 window.aquidauana = aquidauana
